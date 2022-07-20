37da28701d56817f8d98e086f5056b468cd61c1f
10110010_alejandro restrepo montoya <32342594+alejoingenio@users.noreply.github.com>
2022-07-19 23:53:06 -0500

    Add files via upload

---
+++ stage 4.py
@@ -0,0 +1,23 @@
+
+    
+animals = ["camel","lion","deer","goose","bat","rabbit"]
+print('Please enter the number of the habitat you would like to view:')
+user = input()
+while True:
+    print(animals[int(user)])
+    print('Please enter the number of the habitat you would like to view:')
+    user = input()
+    if user == "exit":
+        print('See you later!')
+        break
+
+    
+
+
+
+
+
+        
+        
+            
+
